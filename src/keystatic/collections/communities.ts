7f320a53becf48e3f4ab4b9efb5bf35c70f81601
Agustin Tornielli <agustintornielli25@gmail.com>
2025-04-07 21:49:00 -0300

    feat: Add new collections and utility functions for community, staff, and speaker management, along with interactive UI components

---
+++ src/keystatic/collections/communities.ts
@@ -0,0 +1,30 @@
+import { collection, fields } from "@keystatic/core";
+
+export const communities = collection({
+  label: "Communities",
+  slugField: "name",
+  schema: {
+    name: fields.slug({
+      name: {
+        label: "Name",
+        validation: { isRequired: true },
+      },
+    }),
+    picture: fields.image({
+      label: "Picture",
+      directory: "public/static/communities",
+      publicPath: "/static/communities/",
+      validation: { isRequired: true },
+    }),
+    website: fields.text({
+      label: "Website URL",
+      validation: { isRequired: false },
+    }),
+    description: fields.text({
+      label: "Description",
+      validation: { isRequired: false },
+      multiline: true,
+    }),
+  },
+  path: "content/communities/*/",
+});
