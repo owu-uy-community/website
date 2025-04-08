7caae08e9e58b0f1f1809dfb061956fb4ac90053
Agustin Tornielli <agustintornielli25@gmail.com>
2025-04-07 21:46:32 -0300

    refactor: Remove Payload CMS

--- next.config.js
+++ next.config.js
@@ -3,17 +3,14 @@ require("dotenv").config();
 module.exports = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ["localhost"],
   },
-  experimental: {
-    outputFileTracingIncludes: {
-      "/keystatic/[[...params]]": ["./content/**/*"],
-      "/keystatic": ["./content/**/*"],
-      "/api/keystatic/[...params]": ["./content/**/*"],
-      "/la-meetup": ["./content/**/*"],
-    },
+  outputFileTracingIncludes: {
+    "/keystatic/[[...params]]": ["./content/**/*"],
+    "/keystatic": ["./content/**/*"],
+    "/api/keystatic/[...params]": ["./content/**/*"],
+    "/la-meetup": ["./content/**/*"],
   },
   webpack(config) {
     config.module.rules.push({
