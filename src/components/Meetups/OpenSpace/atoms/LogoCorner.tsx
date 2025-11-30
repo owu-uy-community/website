52a79faa4cc63191c283e7ebc55fee0328965f53
Agustín Tornielli <agustintornielli25@gmail.com>
2025-11-30 17:18:09 -0300

    refactor(logo): replace OWU logo image format and remove unused PNG file (#155)
    - Updated the LogoCorner component to use a WebP format for the OWU logo, improving loading performance and image quality.
    - Deleted the old PNG logo file to clean up the project and reduce unnecessary assets.

--- src/components/Meetups/OpenSpace/atoms/LogoCorner.tsx
+++ src/components/Meetups/OpenSpace/atoms/LogoCorner.tsx
@@ -8,10 +8,9 @@ import * as React from "react";
 export const LogoCorner: React.FC = () => {
   return (
     <div className="absolute -bottom-2 right-0 z-20 max-w-[200px] -rotate-12 sm:-bottom-3 md:-bottom-5">
-      <img src="/images/logos/owu.png" alt="OWU" className="h-16 w-auto object-contain sm:h-24 md:h-32 lg:h-40" />
+      <img src="/images/logos/owu.webp" alt="OWU" className="h-16 w-auto object-contain sm:h-24 md:h-32 lg:h-40" />
     </div>
   );
 };
 
 LogoCorner.displayName = "LogoCorner";
-
